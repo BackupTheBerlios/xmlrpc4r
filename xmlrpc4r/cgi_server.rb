bb8e62b727b7eac0bf1544263a54e22eed810768
michael <michael>
2001-01-24 18:01:00 +0000

    corrected synopsis changed class, now no Singleton is used, insted a class-variable @@init corrected wrong $ENV to ENV

--- xmlrpc4r/cgi_server.rb
+++ xmlrpc4r/cgi_server.rb
@@ -5,7 +5,7 @@
 # 
 # Copyright (C) 2001 by Michael Neumann (neumann@s-direktnet.de)
 #
-# $Id: cgi_server.rb,v 1.1 2001/01/24 17:07:37 michael Exp $
+# $Id: cgi_server.rb,v 1.2 2001/01/24 18:01:00 michael Exp $
 #
 
 
@@ -14,36 +14,40 @@
    require "cgi_server.rb"
    require "create.rb"
  
-   s = CGI_Server.instance     
-   case s.method        # name of called method
+   s = CGI_Server.new     
+   resp = case s.method        # name of called method
    when "michael.add"
      a,b = s.params
-     puts createMethodResponse(true, a+b) 
+     createMethodResponse(true, a+b) 
    when "michael.div"
      a,b = s.params 
-     resp = if b == 0 then
+     if b == 0 then
        createMethodResponse(false, {"faultCode" => 1, 
                             "faultString" => "division by zero"})
      else
        createMethodResponse(true, a/b) 
      end
-     puts resp
    end 
+
+   puts "Content-type: text/xml\n"
+   puts resp
 =end
 
-require "singleton"
-require "parser"
-require "create"
+require "parser.rb"
+require "create.rb"
 
 class CGI_Server
-  include Singleton
+  @@init = false
 
   attr_reader :method, :params
   
   def initialize
-    $stdin.binmode
-    data = $stdin.read(Integer($ENV['CONTENT_LENGTH']))
-    @method, @params = parseMethodCall(data) 
+    if not @@init  
+      $stdin.binmode
+      data = $stdin.read(Integer(ENV['CONTENT_LENGTH']))
+      @method, @params = parseMethodCall(data) 
+      @@init = true
+    end
   end
 
 end
