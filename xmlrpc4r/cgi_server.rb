fc9ceba23a57a8fd30117fb754a32d8f647799c7
michael <michael>
2001-01-24 20:05:29 +0000

    changed because of new class Parser

--- xmlrpc4r/cgi_server.rb
+++ xmlrpc4r/cgi_server.rb
@@ -5,7 +5,7 @@
 # 
 # Copyright (C) 2001 by Michael Neumann (neumann@s-direktnet.de)
 #
-# $Id: cgi_server.rb,v 1.3 2001/01/24 19:47:22 michael Exp $
+# $Id: cgi_server.rb,v 1.4 2001/01/24 20:05:29 michael Exp $
 #
 
 
@@ -13,18 +13,20 @@
 = Synopsis
    require "cgi_server.rb"
  
-   s = CGI_Server.new     
+   create = XMLRPC::Create.new
+   s = XMLRPC::CGI_Server.new     
+
    resp = case s.method        # name of called method
    when "michael.add"
      a,b = s.params
-     createMethodResponse(true, a+b) 
+     create.methodResponse(true, a+b) 
    when "michael.div"
      a,b = s.params 
      if b == 0 then
-       createMethodResponse(false, {"faultCode" => 1, 
-                            "faultString" => "division by zero"})
+       create.methodResponse(false, {"faultCode" => 1, 
+                             "faultString" => "division by zero"})
      else
-       createMethodResponse(true, a/b) 
+       create.methodResponse(true, a/b) 
      end
    end 
 
@@ -35,6 +37,8 @@
 require "parser.rb"
 require "create.rb"
 
+module XMLRPC
+
 class CGI_Server
   @@init = false
 
@@ -44,11 +48,13 @@ class CGI_Server
     if not @@init  
       $stdin.binmode
       data = $stdin.read(Integer(ENV['CONTENT_LENGTH']))
-      @method, @params = parseMethodCall(data) 
+      parser = Parser.new
+      @method, @params = parser.parseMethodCall(data) 
       @@init = true
     end
   end
 
 end
 
+end # module
 
