ef313c42386be7730cc8d73a862e7c05571c975d
michael <michael>
2001-02-02 13:38:20 +0000

    added DateTime support

--- xmlrpc4r/lib/create.rb
+++ xmlrpc4r/lib/create.rb
@@ -3,7 +3,7 @@
 # 
 # Copyright (C) 2001 by Michael Neumann (neumann@s-direktnet.de)
 #
-# $Id: create.rb,v 1.12 2001/01/27 19:52:36 michael Exp $
+# $Id: create.rb,v 1.13 2001/02/02 13:38:20 michael Exp $
 #
 
 require "date"
@@ -104,12 +104,16 @@ class Create
       val = case param
       when Fixnum 
 	ele("i4", param.to_s)
+
       when TrueClass, FalseClass
 	ele("boolean", param ? "1" : "0")
+
       when String 
 	ele("string", param)
+
       when Float
 	ele("double", param.to_s)
+
       when Hash
 	# TODO: can a Hash be empty?
 	
@@ -121,6 +125,7 @@ class Create
 	end
 
 	El.new("struct", nil, *h) 
+
       when Array
 	# TODO: can an Array be empty?
 	a = param.collect {|v| conv2value(v) }
@@ -128,17 +133,22 @@ class Create
 	El.new("array", nil,
 	  El.new("data", nil, *a)
 	)
-      when Date, Time
-        # TODO: Time.gm??? .local???
+
+      when Date
         t = param
-        if t.kind_of? Date then
-          ele("dateTime.iso8601", 
-            format("%.4d%02d%02dT00:00:00", t.year, t.month, t.day))
-        else
-          ele("dateTime.iso8601", t.strftime("%Y%m%dT%H:%M:%S"))  
-        end
+        ele("dateTime.iso8601", 
+          format("%.4d%02d%02dT00:00:00", t.year, t.month, t.day))
+
+      when Time
+        ele("dateTime.iso8601", param.strftime("%Y%m%dT%H:%M:%S"))  
+
+      when XMLRPC::DateTime
+        ele("dateTime.iso8601", 
+          format("%.4d%02d%02dT%02d:%02d:%02d", *param.to_a))
+ 
       when XMLRPC::Base64
 	ele("base64", param.encoded) 
+
       else 
 	raise "Wrong type: not yet working!"
       end
