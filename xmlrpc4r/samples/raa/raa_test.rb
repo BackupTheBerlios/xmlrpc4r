35dde3d66f3f38e2a29e8dd2ab43383b9ac96161
michael <michael>
2001-03-23 18:27:31 +0000

    added documentation

--- xmlrpc4r/samples/raa/raa_test.rb
+++ xmlrpc4r/samples/raa/raa_test.rb
@@ -4,7 +4,7 @@
 # This sample demonstrates how to call the XML-RPC interface
 # of RAA (Ruby Application Archive)
 #
-# $Id: raa_test.rb,v 1.1 2001/03/22 20:07:20 michael Exp $
+# $Id: raa_test.rb,v 1.2 2001/03/23 18:27:31 michael Exp $
 #
 
 require "xmlrpc/client"
@@ -12,18 +12,50 @@ require "xmlrpc/client"
 server = XMLRPC::Client.new("www.ruby-lang.org", "/~nahi/xmlrpc/raa/")
 raa = server.proxy("raa")
 
-klass = Struct.new( "Category" , :major, :minor)
-category = klass.new( "Library", "XML")
 
 
-
+#
+# Returns an array of all names (strings) 
+# of the packages at RAA
+#
 p raa.getAllListings
+
+
+
+#
+# Returns a hash, containing the main sections of RAA as keys
+# (Application, Library, Documentation, Ports...) and the
+# corresponding values are also hashs which values are
+# the subsections, the values are arrays containing the
+# names of the packages under this section.
+#
 p raa.getProductTree
 
 
 
-p raa.getInfoFromCategory( category )
-p raa.getModifiedInfoSince( Time.at( Time.now.to_i - 24 * 3600 ) )
+#
+# Returns an array of hashes, where each hash
+# describes completely a RAA package.
+# Only packages in section "major" and subsection
+# "minor" are returned.
+#
+p raa.getInfoFromCategory( :major => "Library", :minor => "XML" )
+
+
+#
+# The package which name is "XML-RPC" is returned as a
+# hash containing all the info of that package (same as above).
+#
 p raa.getInfoFromName( "XML-RPC" )
+
+
+
+#
+# Get all packages (in an array) which has been
+# modified since the given time.
+#
+p raa.getModifiedInfoSince( Time.at( Time.now.to_i - 24 * 3600 ) )
+
+
 
 
