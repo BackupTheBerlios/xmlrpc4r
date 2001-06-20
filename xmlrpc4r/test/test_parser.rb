8ba302b516b7573f6ecf953d75ee3290e817e4cc
michael <michael>
2001-06-20 09:56:44 +0000

    added new test-case

--- xmlrpc4r/test/test_parser.rb
+++ xmlrpc4r/test/test_parser.rb
@@ -5,7 +5,7 @@
 # 
 # Copyright (C) 2001 by Michael Neumann (neumann@s-direktnet.de)
 #
-# $Id: test_parser.rb,v 1.1 2001/06/11 16:25:15 michael Exp $
+# $Id: test_parser.rb,v 1.2 2001/06/20 09:56:44 michael Exp $
 #
 
 require "runit/testcase"
@@ -15,19 +15,34 @@ require "xmlrpc/parser"
 class Test_Parser < RUNIT::TestCase
 
   def setup
-    @xml = File.readlines("files/xml1.xml").to_s
-    @expected = File.readlines("files/xml1.expected").to_s
+    @xml1 = File.readlines("files/xml1.xml").to_s
+    @expected1 = File.readlines("files/xml1.expected").to_s.chomp
+
+    @xml2 = File.readlines("files/value.xml").to_s
+    @expected2 = File.readlines("files/value.expected").to_s.chomp
   end
 
-  def test_xmlparser
+  def test_xmlparser1
     p = XMLRPC::XMLParser::XMLParser.new
-    assert_equal(p.parseMethodResponse(@xml).inspect, @expected)
+    assert_equal(p.parseMethodResponse(@xml1).inspect, @expected1)
   end
 
-  def test_nqxmlparser
+  def test_nqxmlparser1
     p = XMLRPC::XMLParser::NQXMLParser.new
-    assert_equal(p.parseMethodResponse(@xml).inspect, @expected)
+    assert_equal(p.parseMethodResponse(@xml1).inspect, @expected1)
   end
+
+  # ----------------------------------------------------------
+
+  def test_xmlparser2
+    p = XMLRPC::XMLParser::XMLParser.new
+    assert_equal(p.parseMethodCall(@xml2).inspect, @expected2)
+  end
+
+  def test_nqxmlparser2
+    p = XMLRPC::XMLParser::NQXMLParser.new
+    assert_equal(p.parseMethodCall(@xml2).inspect, @expected2)
+   end
 
 end
 
