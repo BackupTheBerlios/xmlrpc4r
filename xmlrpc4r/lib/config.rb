d34ed29a1d26ea369b11d6a0a70290b80593dad9
michael <michael>
2001-06-21 11:38:55 +0000

    *

--- xmlrpc4r/lib/config.rb
+++ xmlrpc4r/lib/config.rb
@@ -1,5 +1,5 @@
 #
-# $Id: config.rb,v 1.5 2001/06/21 11:38:12 michael Exp $
+# $Id: config.rb,v 1.6 2001/06/21 11:38:55 michael Exp $
 # Configuration file for XML-RPC for Ruby
 #
 
@@ -27,29 +27,6 @@ module XMLRPC
     ENABLE_INTROSPECTION = false
 
   end
-
-
-=begin
-  module XMLWriter
-    DEFAULT_WRITER = Simple # or XMLParser
-  end
-
-  module XMLParser
-    DEFAULT_PARSER = XMLParser # or NQXMLParser
-  end
-
-  module Extensions
-    # enable <nil/> tag
-    ENABLE_NIL_CREATE  = false
-    ENABLE_NIL_PARSER  = false
-    
-    # allows integers greater than 32-bit if true
-    ENABLE_BIGINT      = false
-
-    # enable marshalling ruby objects which include XMLRPC::Marshallable
-    ENABLE_MARSHALLING = true 
-  end
-=end
 
 end
 
