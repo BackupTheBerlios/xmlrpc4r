f8f9ca088400689b9f0fbfe4463ee2fc22d16178
michael <michael>
2001-06-21 19:17:40 +0000

    *

--- xmlrpc4r/samples/marshallable/person.rb
+++ xmlrpc4r/samples/marshallable/person.rb
@@ -1,6 +1,10 @@
 
 class Person 
   include XMLRPC::Marshallable
-  attr_accessor :name, :age  
+  attr_reader :name, :age
+
+  def initialize(name, age)
+    @name, @age = name, age
+  end
 end
 
--- xmlrpc4r/samples/marshallable/server.rb
+++ xmlrpc4r/samples/marshallable/server.rb
@@ -6,11 +6,7 @@ require "person"
 s = XMLRPC::Server.new(8070)
 
 s.add_handler("test.person") {
-  obj = Person.new
-  obj.name = "Michael"
-  obj.age  = 21
-
-  obj
+  Person.new("Michael", 21)
 }
 s.serve
 
